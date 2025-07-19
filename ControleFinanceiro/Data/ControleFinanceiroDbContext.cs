a66de323c932cef19624e24881630356445525c2
Matheus De Alencar <matheusdealencargl@gmail.com>
2025-07-18 21:41:50 -0300

    Conexao com entity framework

---
+++ ControleFinanceiro/Data/ControleFinanceiroDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ControleFinanceiro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleFinanceiro.Data
+{
+    public class ControleFinanceiroDbContext : DbContext
+    {
+        public ControleFinanceiroDbContext(DbContextOptions<ControleFinanceiroDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Cidade> Cidade { get; set; }
+        public DbSet<Conta> Conta { get; set; }
+        public DbSet<Estado> Estado { get; set; }
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
--- ControleFinanceiro/Program.cs
+++ ControleFinanceiro/Program.cs
@@ -1,6 +1,12 @@
+using ControleFinanceiro.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectiontring = builder.Configuration.GetConnectionString("ConnectionPostgres");
+builder.Services.AddDbContext<ControleFinanceiroDbContext>(x => x.UseNpgsql(connectiontring));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
